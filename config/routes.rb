9df36ba1fc9ff8ce880717e8cdd5917063270070
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2017-04-29 16:47:07 +0900

    define create and add in blacklists controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :captured_images, only: [:create]
+      resources :black_lists, only: [:create] do
+         member do post :add end
+      end
     end
   end
 end
